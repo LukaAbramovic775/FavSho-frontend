51fb935bf0f9ed163db535271f0b2c92354b496a
Team-4-prog <60031881+LukaAbramovic775@users.noreply.github.com>
2023-09-02 21:57:11 +0200

    vercel #2

--- src/service/index.js
+++ src/service/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // instanca axios-a za potrebe FavShO backenda
 let Service = axios.create({
-    baseURL: 'fav-sho-backend.vercel.app',
+    baseURL: 'https://fav-sho-backend.vercel.app',
     timeout: 1000,
 });
 
